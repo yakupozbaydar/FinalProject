95721d81ce055f6385331910ac6585c60144fa1f
Yakup Özbaydar <55889068+yakupozbaydar@users.noreply.github.com>
2022-07-29 17:20:42 +0300

    testproject#2
    database connections and some queries

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,13 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
 Console.WriteLine("Hello, World!");
 
-ProductManager productManager = new ProductManager(new InMemoryProductDal());
+ProductManager productManager = new ProductManager(new EfProductDal());
 
 foreach (var product in productManager.GetAll())
 {
     Console.WriteLine(product.ProductName);
 
-}
+}   
--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -7,12 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal
-    {
-        List<Product> GetAll();
-        void Add(Product product);
-        void Update(Product product);
-        void Delete(Product product);
-        List<Product> GetAllByCategory(int categoryId); 
+    public interface IProductDal:IEntityRepository<Product>
+    { 
+        
     }
 }
