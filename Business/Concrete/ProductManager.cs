f7ef6c3ad55142d9c89dd115d787eb2e3f7d42ae
Yakup Özbaydar <55889068+yakupozbaydar@users.noreply.github.com>
2022-08-02 10:22:42 +0300

    11.gün sonu
    .net core api endpointleri yapıldı

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -35,7 +35,7 @@ namespace Business.Concrete
         public IDataResult<List<Product>> GetAll()
         {
             //iş kodları
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 9)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
