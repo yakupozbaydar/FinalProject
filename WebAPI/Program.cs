13089bf43b1468fd12b4a253fa13a9059f5b8d7f
Yakup Özbaydar <55889068+yakupozbaydar@users.noreply.github.com>
2022-08-02 16:33:36 +0300

    final project
    aspect validation(fluent),interception models,product validator

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,14 +1,17 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddSingleton<IProductService,ProductManager>();
-builder.Services.AddSingleton<IProductDal, EfProductDal>();
 builder.Services.AddControllers();
+//builder.Services.AddSingleton<IProductService,ProductManager>();
+//builder.Services.AddSingleton<IProductDal, EfProductDal>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,7 +24,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder=>
+{
+    builder.RegisterModule(new AutofacBusinessModule());
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
